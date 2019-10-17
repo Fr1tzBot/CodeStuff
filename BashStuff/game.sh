89acda4d6a17a723c0f4255674eabca15bdd1899
Fritz Geib <github-f@standland.org>
2019-10-17 13:44:09 -0400

    add bash stuff

---
+++ BashStuff/game.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+echo "Welcome to a super basic text calculator!"
+read -p "enter first number: " first_number
+echo $first_number
+read -p "enter second number:" second_number
+echo $second_number
+read -p "enter operator" operator
+echo $operator
+operators=("+" "-" "*" "/")
+if [$operator in $operators]; then
+	echo "operator is gud"
+	exit
+else
+	echo "operator is not gud"
+	exit
+fi
