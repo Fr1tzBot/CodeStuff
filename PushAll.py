39c0db38ede0e6626ff73197322ad78c549a0fdf
Fritz Geib <github-f@standland.org>
2019-10-23 18:19:02 -0400

    fix pushall error

--- PushAll.py
+++ PushAll.py
@@ -3,6 +3,8 @@ from os import system
 system("git add --all")
 commitname = input("Commit Name: ")
 print(commitname)
-system("git commit -m " + commitname)
+#for error detecting
+system("git commit -m " + str(commitname))
 system("git push origin CloudShell")
 system("git status")
+
