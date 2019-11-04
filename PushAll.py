9f330c7ca358852452707695fb8be12993a9c6df
Fritz Geib <ftgeib640@pccsk12.com>
2019-11-04 11:13:49 -0500

    Python Push

--- PushAll.py
+++ PushAll.py
@@ -2,17 +2,21 @@ from os import system
 from time import sleep
 
 system("git status")
-sleep(5)
+sleep(1)
 system("git add --all")
-sleep(5)
+sleep(1)
 system("git status")
 commitname = input("Commit Name: ")
 print(commitname)
 print(type(commitname))
+sleep(1)
 #for error detecting
 command = "git commit -m '" + str(commitname)
 command = command + "'"
 print(command)
+sleep(1)
+system("git config --global user.email 'ftgeib640@pccsk12.com'")
+system("git config --global user.name 'Fritz Geib'")
 system(command)
 system("git push origin master")
 system("git status")
