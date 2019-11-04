99c7eeab9a7d629cf1cb923f5628c645bf7f97ef
Fritz Geib <github-f@standland.org>
2019-11-04 08:35:23 -0500

    add bash stufs

---
+++ BashStuff/Password.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "Username:" username
+read -sp "Password:" password
+echo
+echo Thank You, $username we now have your login details
+echo $password
---
+++ BashStuff/WhatsYourName.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#ask for name
+echo "What is your name?"
+read name
+echo Nice to meet you, $name
