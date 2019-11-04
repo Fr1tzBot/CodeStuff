97730596174fa3f63ed61c9134d83a6e8ae2bd07
Fritz Geib <ftgeib640@pccsk12.com>
2019-11-04 14:38:22 -0500

    java calculator work

--- JavaStuff/WorkInProgress/Calculator.java
+++ JavaStuff/WorkInProgress/Calculator.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 
 //Work in progress
 //a remake of my python calculator
-public class Calculator2 {
+public class Calculator {
     public static void main(String args[])
         throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         scanner.close(); //to fix warning, remove later
-        System.out.println("welcome to the java interactived calculator!");
-        Thread.sleep(2000);
+        System.out.println("Welcome to my java interactive calculator!");
+        Thread.sleep(1000);
         System.out.println("This calulator will work as a calculator for dividing, multiplying, subtracting, adding, or exponents");
         System.out.println("with two numbers.");
-        Thread.sleep(2000);
-        System.out.println("first you will have to enter your first number, then your operator, and finally, your second number.");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
+        System.out.println("First you will have to enter your first number, then your operator, and finally, your second number.");
+        Thread.sleep(1000);
         Boolean rerun = true;
         int First_number = 0;
         String Second_number = "";
@@ -23,9 +23,10 @@ public class Calculator2 {
         int Second_numberint = 0;
         while(rerun) {
             while(true){
-                //First_number =
+                try{
+                  //todo: fix this
+                }
             }
         }
-        
     }
 }
