6e8a73555c0762454c2132b779441e0d58772c91
Fritz <github-f@standland.org>
2020-01-10 12:48:52 -0500

    work on file delete code

--- PythonStuff/WorkInProgress/FileDeleter.py
+++ PythonStuff/WorkInProgress/FileDeleter.py
@@ -1,12 +1,40 @@
 from os import system
 active = True
+temp = ""
 while active:
 	print("Please Choose Mode of Deleting:")
 	print("[F]Iletype, [D]irectory, or [O]ne file")
 	mode = input("")
 	modes = ["f", "d", "o"]
-	if mode.lower() not in modes:
+	mode = mode.lower()
+	if mode not in modes:
 		continue
 	else:
-		
+		if mode == "f":
+			#delete filetype mode
+			print("what filetype would you like to delete?")
+			fType = input("")
+			print(range(1, len(fType) - 1))
+			if fType[0] == ".":
+				for i in range(1, len(fType)):
+					temp = temp + fType[i]
+				print(temp)
+				fType = temp
+			print("where is this file located?")
+			path = input("")
+			if path == "":
+				path = "~/Documents/CodeStuff/"
+			system("find " + path + " -maxdepth 2 -type f -name  *." + fType + " -delete")
+			active = False
+			break
+		if mode == "d":
+			#delete directory
+			pass
+		if mode == "o":
+			#delete one file
+			pass
+		#
+	#
+#
+
 
