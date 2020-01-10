30d84677a840210653d1f12012cf7aea56189006
Fritz <github-f@standland.org>
2020-01-10 10:22:56 -0500

    add logic

--- PythonStuff/WorkInProgress/FileDeleter.py
+++ PythonStuff/WorkInProgress/FileDeleter.py
@@ -2,5 +2,11 @@ from os import system
 active = True
 while active:
 	print("Please Choose Mode of Deleting:")
-	
+	print("[F]Iletype, [D]irectory, or [O]ne file")
+	mode = input("")
+	modes = ["f", "d", "o"]
+	if mode.lower() not in modes:
+		continue
+	else:
+		
 
