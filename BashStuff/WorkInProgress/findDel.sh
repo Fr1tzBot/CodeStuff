9a750b4049e7ba3060eeb000f632bc65ceea1bc9
Fritz <github-f@standland.org>
2020-01-22 20:12:08 -0500

    work ok findDel

--- BashStuff/WorkInProgress/findDel.sh
+++ BashStuff/WorkInProgress/findDel.sh
@@ -5,15 +5,17 @@ echo 'enter dir path to move to:'
 #read TargetPath
 echo 'enter location of files:'
 #read FileDir
+rm -rf testDir
 testDir () {
     pwd
-    if [test -f "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/test.txt"]; then 
+    if [ test -e "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/test.txt" ]; then 
         #file exists
         echo 'file exists'
     else
         mkdir testDir
         mkdir testDir/testDir2
         touch testDir/testDir2/test.txt
+    fi
 }
 cleanDir () {
     rm -rf testDir
