1ac5026e03ec3621c83b36a860d7c4b144f52940
Fritz <github-f@standland.org>
2020-01-22 22:00:10 -0500

    david is dumb

--- BashStuff/WorkInProgress/findDel.sh
+++ BashStuff/WorkInProgress/findDel.sh
@@ -1,24 +1,29 @@
-echo 'FileDel v1.0 >>>'
-echo 'enter target File extension'
+#echo 'FileDel v1.0 >>>'
+#echo 'enter target File extension'
 #read FileExtension
-echo 'enter dir path to move to:'
+#echo 'enter dir path to move to:'
 #read TargetPath
-echo 'enter location of files:'
+#echo 'enter location of files:'
 #read FileDir
 rm -rf testDir
-testDir () {
-    pwd
-    if [ test -e "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/test.txt" ]; then 
+CreateTestDir () {
+    #if [ test -e "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/test.txt" ]; then 
+        #then
         #file exists
-        echo 'file exists'
-    else
-        mkdir testDir
-        mkdir testDir/testDir2
-        touch testDir/testDir2/test.txt
-    fi
+        #echo 'file exists'
+    #else
+    mkdir testDir
+    mkdir testDir/testDir2
+    touch testDir/testDir2/test.txt
+    touch testDir/testDir2/test2.txt
+    echo "created test dir"
+    #fi
 }
 cleanDir () {
     rm -rf testDir
+    echo "removed test dir"
 }
-#find FileDir -name $FileExtension -exec mv {} $TargetPath \;
-testDir
+CreateTestDir
+find "testDir/testDir2" -name "*.txt" -type f -exec mv {} "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/" \;
+#find "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/testDir2/"" -name "*.txt" -type f -delete
+echo 'remove all .txt files'
