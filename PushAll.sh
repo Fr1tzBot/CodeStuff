468920b9898e5d99b2717cc173fe1dcc59ab170c
Fritz <github-f@standland.org>
2020-01-24 10:40:02 -0500

    update pushall

--- PushAll.sh
+++ PushAll.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 sh JavaStuff/CleanClass.sh
-git status
-sleep 1
 git add --all
 git status
-sleep 1
-read -p "Commit Name:" commit
-echo commit
+read -p "Commit Name: " commit
 git commit -m "$commit"
 git push origin master
