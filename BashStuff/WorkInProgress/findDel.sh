7b9c8706d21e2b4aed3839414c6fd1223ee70c12
Fritz <github-f@standland.org>
2020-01-24 08:34:56 -0500

    epmkwepm

--- BashStuff/WorkInProgress/findDel.sh
+++ BashStuff/WorkInProgress/findDel.sh
@@ -1,8 +1,8 @@
-#echo 'FileDel v1.0 >>>'
-#echo 'enter target File extension'
-#read FileExtension
-#echo 'enter dir path to move to:'
-#read TargetPath
+echo 'FileDel v1.0 >>>'
+read -p "Target File Extension:" FileExtension
+StarDot="*."
+FileExtension=$StarDot$FileExtension
+#read -p "Path to Copy to:" TargetPath
 #echo 'enter location of files:'
 #read FileDir
 rm -rf testDir
@@ -12,19 +12,25 @@ CreateTestDir () {
         #file exists
         #echo 'file exists'
     #else
+    cd ~/Documents/CodeStuff/BashStuff/WorkInProgress/
     mkdir testDir
     mkdir testDir/testDir2
     touch testDir/testDir2/test.txt
     mkdir testDir/testDir3
     touch testDir/testDir3/test2.txt
+    mkdir copyTo
     echo "created test dir"
     #fi
 }
 cleanDir () {
+    cd ~/Documents/CodeStuff/BashStuff/WorkInProgress/
     rm -rf testDir
+    rm -rf copyTo
     echo "removed test dir"
 }
+cleanDir
 CreateTestDir
-find "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/" -name "*.txt" -type f -exec mv {} "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/" \;
+find ~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/ -name $FileExtension -type f -exec mv {} ~/Documents/CodeStuff/BashStuff/WorkInProgress/copyTo/ \;
+find ~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/ -type d -delete
 #find "~/Documents/CodeStuff/BashStuff/WorkInProgress/testDir/testDir2/"" -name "*.txt" -type f -delete
-echo 'remove all .txt files'
+echo 'move all .txt files'
