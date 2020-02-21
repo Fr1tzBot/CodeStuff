311b5b5a88f6af99066e003574fc27c21fda0fe1
Fritz <github-f@standland.org>
2020-02-21 08:01:41 -0500

    add Snake Math

---
+++ PythonStuff/WorkInProgress/SnakeMath.py
@@ -0,0 +1,93 @@
+from time import sleep
+import random
+
+screenLength = 32
+screenWidth = 8
+
+headY = screenWidth/2
+headX = screenLength/2
+headY = int(headY)
+headX = int(headX)
+headPos = [headX, headY]
+snakeDir = random.randint(0, 7)
+SnakeLength = 1
+Score = 0
+
+appleX = 0
+appleY = 0
+applePos = [appleX, appleY]
+
+allDirs   =  [0, 1, 2, 3, 4, 5, 6, 7]
+upDirs    =  [0, 1, 2]
+rightDirs =  [2, 3, 4]
+leftDirs  =  [0, 7, 6]
+downDirs  =  [6, 5, 4]
+
+#HeadDirs
+# 0  1  2
+# 7     3
+# 6  5  4
+
+def drawPixel(x, y):
+    #draw a whitePixel
+    #print("Dreweds X: " + str(x) + " Y: " + str(y))
+    pass
+def redPixel(x, y):
+    #draw a red pixel
+    pass
+def clearPixel(z,x,y):
+    #print("cleared X: " + str(x) + " Y: " + str(y))
+    pass
+def killSnake():
+    #end the game here
+    print("you lost,")
+    print("therefore, you are a loser.")
+    sleep(1)
+    exit()
+def getApplePos():
+    return [random.randint(0, screenLength), random.randint(0, screenWidth)]
+def updateHeadPos():
+    global headX
+    global headY
+    global headPos
+    global screenLength
+    global snakeDir
+    if headX == 0 and snakeDir in leftDirs and headX == screenLength and snakeDir in rightDirs:
+        killSnake()
+    elif headY == 0 and snakeDir in downDirs or headY == screenWidth and snakeDir in upDirs:
+        killSnake()
+    elif snakeDir == 0:
+        headX -= 1
+        headY += 1
+    elif snakeDir == 1:
+        headY += 1
+    elif snakeDir == 2:
+        headX += 1
+        headY += 1
+    elif snakeDir == 3:
+        headX += 1
+    elif snakeDir == 4:
+        headX += 1
+        headY -= 1
+    elif snakeDir == 5:
+        headY -= 1
+    elif snakeDir == 6:
+        headX -= 1
+        headY -= 1
+    elif snakeDir == 7:
+        headX -= 1
+def updateSnakeDir():
+    global snakeDir
+    for i in range(len(allDirs)):
+        if allDirs[i] == snakeDir and i == len(allDirs):
+            snakeDir = allDirs[0]
+        elif allDirs[i] == snakeDir:
+            snakeDir = allDirs[snakeDir + 1]
+while True:
+    applePos = getApplePos()
+    headPos = [headX, headY]
+    drawPixel(headPos[0], headPos[1])
+    updateHeadPos()
+    drawPixel(applePos[0], applePos[1])
+    updateSnakeDir()
+    print(snakeDir)
