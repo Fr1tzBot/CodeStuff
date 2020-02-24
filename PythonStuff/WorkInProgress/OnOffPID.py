0f4c646eb74a13166ac89b864746a3a8429b4ffc
Fritz <github-f@standland.org>
2020-02-24 14:38:18 -0500

    YEET

--- PythonStuff/WorkInProgress/OnOffPID.py
+++ PythonStuff/WorkInProgress/OnOffPID.py
@@ -1,5 +1,10 @@
+#%matplotlib inline
 from time import sleep
+import time
 import random
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
 
 target = 50
 Kp = 5
@@ -10,19 +15,19 @@ error = 0
 def getData():
     global data
     if random.randint(0, 100) == 69:
-        print("NOICE!")
+        #print("NOICE!")
         data = data * 2
         if data > 100:
             data = 100
     elif isOn:
-        if random.randint(1, 2) == 1:
+        if random.randint(1, 5) == 1:
             data += 1
         else:
             data += 2
     elif data == 0:
         pass
     else:
-        data -= random.randint(1, 2)
+        data -= random.randint(1, 5)
     return data
 def setCondition(error, Kp):
     global isOn
@@ -31,12 +36,17 @@ def setCondition(error, Kp):
         isOn = True
     elif calc < 0:
         isOn = False
-while True:
+for i in range(100):
+    if i == 0:
+        start_time = time.time()
     error = target - getData()
     setCondition(error, Kp)
-    print("E: " + str(error) + " On?: " + str(isOn) + " Data: " + str(data))
+    #print("E: " + str(error) + " On?: " + str(isOn) + " Data: " + str(data))
     if data < 0 or data > 100:
         print("Out Of Bounds Error 969")
         exit()
-    sleep(0.1)
+    plt.plot(time.time() - start_time, data, '-ok')
+
+    #sleep(0.1)
+plt.show()
     
