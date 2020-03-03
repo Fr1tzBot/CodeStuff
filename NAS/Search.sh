05282c1b78165e19e1ee924195fde7d166d1a5a5
Fritz <github-f@standland.org>
2020-03-03 12:33:02 -0500

    que?

--- NAS/Search.sh
+++ NAS/Search.sh
@@ -5,6 +5,7 @@ if [[ -f "$passwordPath" ]]; then
     echo "Beginning SSH Init..."
     password=$(head -n 1 $passwordPath)
     sshpass -p $password ssh root@freenas.local cd "$defaultPath"
+    sshpass -p $password ssh root@freenas.local sudo /usr/libexec/locate.updatedb
     echo "SSH Init Complete."
 else
     echo "Permission Denied: /home/fritz/password not found."
