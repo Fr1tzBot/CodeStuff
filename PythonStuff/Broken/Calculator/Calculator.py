9d5dc692fd769b40c7ccf93be2658c4e15005386
Fritz <github-f@standland.org>
2020-03-09 08:27:18 -0400

    calculator 2.0

--- PythonStuff/Broken/Calculator/Calculator.py
+++ PythonStuff/Broken/Calculator/Calculator.py
@@ -3,22 +3,12 @@ time.sleep(1)
 print("welcome to the python interactived calculator!")
 time.sleep(1)
 print("This calulator will work as a calculator for dividing, multiplying, subtracting, or adding two numbers.")
-time.sleep(1)
+time.sleep(1.5)
 print("first you will have to enter your first number, then your operator, and finally, your second number.")
-time.sleep(1)
+time.sleep(1.55)
 first_number = input("What is your first number?")
-time.sleep(1)
-if type(first_number) != "<type 'int'>" or type(second_number) != "<type 'int'>":
-  print("you must enter a number")
-  time.sleep(1)
-  quit()
 operator = input("what is your operator?")
-time.sleep(1)
 second_number = input("What is your second number?")
-if type(first_number) != "<type 'int'>" or type(second_number) != "<type 'int'>":
-  print("you must enter a number")
-  time.sleep(1)
-  quit()
 if operator == "*" :
   answer = int(first_number) * int(second_number)
   print(answer)
@@ -26,8 +16,15 @@ if operator == "/":
   answer = int(first_number) / int(second_number)
   print(answer)
 if operator == "+":
-  answer = float(first_number) + float(second_number)
-  print(answer)
+  try:
+    answer = float(first_number) + float(second_number)
+    print(answer)
+  except ValueError:
+    print("you must enter a number")
 if operator == "-":
   answer = float(first_number) - float(second_number)
   print(answer)
+  time.sleep(1.5)
+  quit()
+
+
