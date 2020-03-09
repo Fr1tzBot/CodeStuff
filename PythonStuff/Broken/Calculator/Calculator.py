a253e66649cdca060379df0312dbf4eefc00c24a
Fritz Geib <ftgeib640@pccsk12.com>
2020-03-09 08:32:47 -0400

    calculator 5.0

--- PythonStuff/Broken/Calculator/Calculator.py
+++ PythonStuff/Broken/Calculator/Calculator.py
@@ -7,27 +7,29 @@ time.sleep(1.5)
 print("first you will have to enter your first number, then your operator, and finally, your second number.")
 time.sleep(1.55)
 while True:
-  first_number = raw_input("What is your first number?")
-  if type(first_number) != "<type 'int'>":
+  try:
+    first_number = float(input("What is your first number?"))
+  except ValueError:
     print("you must enter a number")
     time.sleep(1)
     continue
   operator = input("what is your operator?")
-  if operator != "-" or "+" or "/" or "*":
+  if operator not in "*, /, +, -":
     print("your operator must be either a +,-,/, or *")
     time.sleep(1.8)
     continue
-  second_number = raw_input("What is your second number?")
-  if type(second_number) != "<type 'int'>":
+  try:
+    second_number = float(input("What is your second number?"))
+  except ValueError:
     print("you must enter a number")
     time.sleep(1)
     continue
   break
 if operator == "*" :
-  answer = int(first_number) * int(second_number)
+  answer = float(first_number) * float(second_number)
   print(answer)
 if operator == "/":
-  answer = int(first_number) / int(second_number)
+  answer = float(first_number) / float(second_number)
   print(answer)
 if operator == "+":
     answer = float(first_number) + float(second_number)
