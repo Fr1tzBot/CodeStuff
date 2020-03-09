0cd7a98977f6210e2cb44c90e387282965892650
Fritz Geib <ftgeib640@pccsk12.com>
2020-03-09 08:33:19 -0400

    calculator 5.1

--- PythonStuff/Broken/Calculator/Calculator.py
+++ PythonStuff/Broken/Calculator/Calculator.py
@@ -1,12 +1,18 @@
 import time
 time.sleep(1)
-print("welcome to the python interactived calculator!")
+print("welcome to the python interactived calculator 6.0!")
 time.sleep(1)
 print("This calulator will work as a calculator for dividing, multiplying, subtracting, or adding two numbers.")
 time.sleep(1.5)
 print("first you will have to enter your first number, then your operator, and finally, your second number.")
 time.sleep(1.55)
 while True:
+  try:
+    total_numbers = float(input("how many numbers would you like to operate with?"))
+  except ValueError:
+    print("you must enter a number")
+    time.sleep(1)
+    continue
   try:
     first_number = float(input("What is your first number?"))
   except ValueError:
