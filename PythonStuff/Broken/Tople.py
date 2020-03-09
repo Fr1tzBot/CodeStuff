964ac877ebe8c590d4366a62d8ebcd74cb3e8122
Fritz <github-f@standland.org>
2020-03-09 09:28:49 -0400

    more cleanup

--- PythonStuff/Broken/Tople.py
+++
@@ -1,8 +0,0 @@
-def tople(t):
-  counter = 0
-  while not counter >= len(t):
-    tople = tuple()
-    tople = tople + (int(t[counter]),)
-    counter = counter + 1
-  tople = tople + (int(t[len(t)-1]),)
-  return tople
