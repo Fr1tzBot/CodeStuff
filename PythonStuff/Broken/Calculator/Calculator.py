95344fc2afb104e879a524e739d372ed01a69e05
Fritz Geib <ftgeib640@pccsk12.com>
2020-03-09 08:36:28 -0400

    calculator 7.0

--- PythonStuff/Broken/Calculator/Calculator.py
+++ PythonStuff/Broken/Calculator/Calculator.py
@@ -8,6 +8,8 @@ print("first you will have to enter your first number, then your operator, and f
 time.sleep(2)
 print("New in version 6.0! square roots. Type sqrt or square root in the operator menu.")
 time.sleep(2)
+print("Now with factorials! type a ! in the operator menu!")
+time.sleep(3)
 while True:
   try:
     first_number = float(input("What is your first number?"))
@@ -18,7 +20,9 @@ while True:
   operator = input("what is your operator?")
   if operator == "square root" or operator == "sqrt":
     break
-  if operator not in "*, /, +, -, ^":
+  if operator == "!":
+    break
+  if operator not in "*, /, +, -, ^, !":
     print("your operator must be either a +,-,/, or *")
     time.sleep(1.8)
     continue
@@ -41,4 +45,6 @@ if operator == "square root" or operator == "sqrt":
   answer = math.sqrt(first_number)
 if operator == "^":
   answer = (float(first_number) ** float((second_number)))
-print(answer)  
+if operator == "!":
+  answer = math.factorial(float(first_number))
+print(answer)
