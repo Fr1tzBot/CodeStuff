2bd96b3071e64b8fb61907449cb9c6e36c5e4a00
Fritz <github-f@standland.org>
2020-05-09 17:26:03 -0400

    add dependency installer

---
+++ PythonStuff/WorkInProgress/DependencyInstaller.sh
@@ -0,0 +1,10 @@
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install beautifulsoup4
+pip3 install google
+pip3 install youtube_dl
+pip3 install urllib3
+pip3 install pydub
+pip3 install glob3
+pip3 install python-vlc
+pip install mutagen
