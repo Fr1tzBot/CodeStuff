f2de908b8c2ef9a1c88fab1ca3336243c4bc058c
Fritz <github-f@standland.org>
2020-07-28 09:24:26 -0400

    It looks good now

--- PythonStuff/Working/TkinterTest.py
+++ PythonStuff/Working/TkinterTest.py
@@ -4,34 +4,43 @@ window = Tk()
 
 window.title("MusicDownloader")
 
-window.geometry('250x350')
+window.geometry('')
 window.bind('<Return>', exit)
 
-#Row One:
-def readTitle():
-    print(title.entry.get())
+def readAll():
+    title.value = title.entry.get()
+    artist.value = artist.entry.get()
+
+class intro:
+    row = 0
+    version = 1.7
+    label = Label(window, text="Music Downloader Version " + str(version))
+intro.label.grid(column=0, row=intro.row)
+blank = Label(window, text="")
+blank.grid(column=0, row = intro.row+1)
 
 class title:
-    row = 0
+    value = ""
+    row = 2
     label = Label(window, text="Title:")
-    entry = Entry(window,width=15)
-    button = Button(window, text="", command=readTitle)
+    entry = Entry(window,width=30)
 
 title.label.grid(column=0, row=title.row)
-title.entry.grid(column=1, row=title.row)
-title.button.grid(column=2, row=title.row)
+title.entry.grid(column=0, row=title.row+1)
 
-#Row Two:
-def readArtist():
-    print(artist.entry.get())
 class artist:
-    row = 3
+    value = ""
+    row = 4
     label = Label(window, text="Artist:")
-    entry = Entry(window, width = 15)
-    button = Button(window, text="", command=readArtist)
+    entry = Entry(window, width = 30)
 
 artist.label.grid(column=0, row=artist.row)
-artist.entry.grid(column=1, row=artist.row)
-artist.button.grid(column=2, row=artist.row)
+artist.entry.grid(column=0, row=artist.row+1)
+
+blank = Label(window, text="")
+blank.grid(column=0, row=artist.row+3)
+
+nextButton = Button(window, text="Next", command=readAll)
+nextButton.grid(column=0, row=artist.row+4, sticky=SE)
 
 window.mainloop()
