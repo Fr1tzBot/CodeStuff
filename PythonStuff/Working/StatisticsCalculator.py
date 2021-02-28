0fc69bbc4799ef83947895e5c030f2785f8e2d50
Fritz Geib <fritz@standland.org>
2021-02-27 22:17:06 -0500

    Fix some Ancient Spaghetti code

--- PythonStuff/Working/StatisticsCalculator.py
+++ PythonStuff/Working/StatisticsCalculator.py
@@ -1,121 +1,69 @@
-#import neccesary modules
-from time import sleep
 #define afternumber function
-def afternumber(i):
-  st = (1,21,31,41,51,61,71,81,91,101,121)
-  rd = (3,23,33,43,53,63,73,83,93,103,123)
-  nd = (2,22,32,42,52,62,72,82,92,102,122)
-  if i in st:
+def afternumber(number):
+  if str(number)[-1] == "1":
     return "st"
-  elif i in rd:
+  elif str(number)[-1] == "3":
     return "rd"
-  elif i in nd:
+  elif str(number)[-1] == "2":
     return "nd"
   else:
     return "th"
 #define mode function
-def mode(arr) :
-    m = max([arr.count(a) for a in arr])
-    return [x for x in arr if arr.count(x) == m][0] if m>1 else None
+def mode(arr):
+  m = max([arr.count(a) for a in arr])
+  return [x for x in arr if arr.count(x) == m][0] if m>1 else None
 #define mad function
 def mad(d, t):
   average = t / len(d)
   counter = 0
   tmad = 0
   while counter != len(d): 
-      tmad = tmad + abs((d[counter]) - average)
-      counter += 1
+    tmad = tmad + abs((d[counter]) - average)
+    counter += 1
   mad = tmad / len(d)
   return mad
-#define total of tuple function
-def total(i):
-  count = int(len(i))
-  t = int()
-  while count != 0:
-    t = float(t) + float(i[count - 1])
-    count = count - 1
-  if type(t) == "integer":
-    t = int(t)
-  return(t)
-#define tople function
-def tople(t):
-  counter = int(0)
-  length = len(t)
-  out = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-  toremove = len(out) - length
-  counter = toremove
-  while len(out) != length:
-    out.remove(0)
-    counter -= 1
-  counter = 0
-  while counter != length:
-      out[counter] = (int(t[counter]))
-      counter += 1
-  return out
 #define median function
 def median(l):
-    l = tuple(l)
-    half = len(l) // 2
-    if not len(l) % 2:
-        return (l[half - 1] + l[half]) / 2.0
-    return l[half]
-#ask for the amount of digits
-try:
-  number = int(input("how many numbers do you have? "))
-#return error if user doesn't enter a number
-except ValueError:
-  print("you must enter a number")
-  sleep(1)
-  exit()
-#format all variables
-counter = int()
-data = list()
+  half = len(l) // 2
+  if not len(l) % 2:
+    return (l[half - 1] + l[half]) / 2.0
+  return l[half]
 #find number of numbers
-while counter != number:
-  after = afternumber(counter + 1)
-  data = (data + list(input("what is your " + (str(counter + 1)) + str(after) + " number? ")))
-  counter = counter + 1
-#prevent strange errors #i don't know why this works but it does
-if len(data) != number:
-    print("fatal error:ok")
-    sleep(1)
-    print("two or more digits will be imported in future versions")
-    sleep(2)
+data = input("input your data in [1,0,1] format (spaces will be removed) ")
+toRemove = [" ", "[", "]", "(", ")"]
+for i in toRemove:
+  data = data.replace(i, "")
+data = data.split(",")
+for i in range(len(data)):
+  try:
+    data[i] = float(data[i])
+  except ValueError:
+    print("You Must Enter a Number.")
     exit()
+
 #format data as tuple using tople function  #sort the data
-data = sorted(tople(data))
+data = sorted(data)
+
 #show the data
 print(data)
-sleep(1)
+
 #calculate the total
-print("calculating total...")
+print("Calculating total...")
+
 #format total variable
-sleep(.5)
-print("total is " + str(total(data)))
+print("Total is " + str(sum(data)))
+
 #calculate the mean
-sleep(.5)
-print("calculating average...")
-sleep(.5)
-print ("average is " + str((total(data)) / number))
-sleep(.5)
+print ("Average is " + str((sum(data)) / len(data)))
+
 #calculate the median
-print("calculating median...")
-sleep(.5)
-print("median is " + str(median(data)))
-sleep(.5)
+print("Median is " + str(median(data)))
+
 #calculate the MAD
-print("calculating MAD...")
-sleep(.5)
-print("MAD is " + str(mad(data, (total(data)))))
-sleep(.5)
-print("calculating mode...")
-sleep(.5)
-print("mode is " + str(mode(data)))
-sleep(.5)
-print("calculating Maximum...")
-sleep(.5)
-print("MAximum is " + str(max(data)))
-sleep(.5)
-print("Calculating Minimum...")
-sleep(.5)
-print("minimum is " + str(min(data)))
+print("MAD is " + str(mad(data, (sum(data)))))
+
+print("Mode is " + str(mode(data)))
+
+print("Maximum is " + str(max(data)))
+
+print("Minimum is " + str(min(data)))
