6b82e36e62d438baf7717dac4f3cba00df7b92cf
Albert Chen <chenalbert06@gmail.com>
2021-06-18 17:27:34 -0400

    fixed scrolling text

--- Python/Working/ScrollingText.py
+++ Python/Working/ScrollingText.py
@@ -1,18 +1,20 @@
 from time import sleep
 
 def scrollPrint(text, delay):
-    letterList = []
     for i in range(len(text)):
-        letterList.append(text[i])
-    for i in range(len(text)):
+        print(text[:i+1], end = '\r')
         sleep(delay)
-        print(letterList[i], end="")
     print("\n")
+    
 def wordScroll(text, delay):
+    currenttext = ""
     wordList = text.split()
-    for i in wordList:
+    for word in wordList:
+        currenttext = currenttext + word + " "
+        print(currenttext, end = '\r')
+
         sleep(delay)
-        print(i + " ", end="")
     print("\n")
+    
 scrollPrint("Hello World", 0.5)
 wordScroll("Shall we play a game?", 1.5)
