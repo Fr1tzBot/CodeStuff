28c4b54c52d4db97830300f0cb803a01bb98ddc3
Fritz Geib <fritz@standland.org>
2021-08-22 00:33:10 -0400

    c is specia

--- Reference/c/whatsyourname.c
+++ Reference/c/whatsyourname.c
@@ -2,7 +2,7 @@
 int main() {
     char firstName[100];
     printf("Type your first name: ");
-    scanf("%s", &firstName);
+    scanf("%s", firstName);
     printf("Nice To Meet You %s!\n", firstName);
     return 0;
 }
