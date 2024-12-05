fea89a06ccd6558f2e414625a6605d56313d80c8
Fritz Geib <fritz@standland.org>
2024-12-05 08:12:54 -0500

    comments!

--- matlab/demo3/oxygenDataParser.m
+++ matlab/demo3/oxygenDataParser.m
@@ -1,14 +1,22 @@
+% oxygenDataParser.m: parses sample oxygen data, estimates the status of the provided astronauts, and toggles LEDs to reflect their status
+% Author: Fritz Geib
+% Date: 12/5/2024
+
+% set up workspace
 clc
 clear all
 
 % Load the data
 [time, person1, person2] = readvars('DemoP1.xlsx');
 
+% initialize state machines and buffers for both people
 p1status = "";
 p2status = "";
 p1buffer = "";
 p2buffer = "";
 
+%parseplayer: returns a matrix containing two elements, first the green led pin, then the red led pin
+%@param player: the player number (1 or 2)
 function pins = parseplayer(player)
     if player == 1
         pins = ["D8", "D10"];
@@ -17,6 +25,11 @@ function pins = parseplayer(player)
     end
 end
 
+%statustoled: toggle the appropriate pins on the arduino based on the provided status
+%@param a: the arduino object
+%@param status: the status of the player
+%@param player: the player number (1 or 2)
+%@see parseplayer
 function statustoled(a, status, player)
     pins = parseplayer(player);
     if status == "eva"
@@ -28,6 +41,12 @@ function statustoled(a, status, player)
     end
 end
 
+%printstatus: prints the status of the person and triggers an led update if the status has changed
+%@param a: the arduino object
+%@param person: the person number (1 or 2)
+%@param status: the current status of the person
+%@param buffer: the previous status of the person
+%@param t: the current time
 function printstatus(a, person, status, buffer, t)
     if status ~= buffer
         if status == "awake" && buffer == "sleep"
@@ -48,15 +67,20 @@ function printstatus(a, person, status, buffer, t)
     end
 end
 
-UPPER_THRESHOLD = 0.55;
-LOWER_THRESHOLD = 0.38;
-GROUND_PINS = ["D3", "D5", "D9", "D11"];
+% Constants
+UPPER_THRESHOLD = 0.55; % o2 value above this is an EVA
+LOWER_THRESHOLD = 0.38; % o2 value below this is sleeping
+GROUND_PINS = ["D3", "D5", "D9", "D11"]; % pins to ground the LEDs
 
+% Initialize the arduino
 a = arduino();
+
+% Ground the LEDs
 for i=1:length(GROUND_PINS)
     a.writeDigitalPin(GROUND_PINS(i), 0)
 end
 
+% removed plotting code
 % hold on
 % plot(time, person1, 'r');
 % plot(time, person2, 'b');
@@ -97,8 +121,10 @@ for i = 1:length(time)
         p2status = "awake";
     end
 
+    % Print the status of both people
     printstatus(a, 1, p1status, p1buffer, t)
     printstatus(a, 2, p2status, p2buffer, t)
 
+    % Pause for a moment to simulate real-time data
     pause(0.02)
 end
