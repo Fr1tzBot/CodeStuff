da3ef7b26337385fbf83ed1f82270ad4168159b6
Fritz Geib <fritz@standland.org>
2025-03-20 17:49:59 -0400

    new classes!

--- python/classregister.py
+++ python/classregister.py
@@ -9,7 +9,7 @@ sleep(1)
 print("1")
 sleep(1)
 
-for i in "13435 11896 10111 11233 12620 13135 12510".split(" "):
+for i in "81227 84241 85130 80071 83294 83830 82516 82765 82239".split(" "):
     for j in i:
         keyboard.press(j)
         keyboard.release(j)
