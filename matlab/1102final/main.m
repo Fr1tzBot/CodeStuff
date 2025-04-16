49d9ed359a4a7ef3fc1f2c577c85ddfc3070df8b
Fritz Geib <fritz@standland.org>
2025-04-15 23:17:03 -0400

    expo changed

--- matlab/1102final/main.m
+++ matlab/1102final/main.m
@@ -18,41 +18,40 @@ crushedGrain = mill(recipe.grain);
 %Brew Kettle / Mashing
 wort = mash(a, crushedGrain);
 pump(a, true)
-pause(10)
+pause(5)
 pump(a, false)
 
 %Lauter Tun
 wort = lauter(a, crushedGrain, wort);
 pump(a, true)
-pause(10)
+pause(5)
 pump(a, false)
 
 %Boil Kettle
 wort = boil(a, recipe, wort);
 pump(a, true)
-pause(10)
+pause(5)
 pump(a, false)
 
 %Whirlpool
 wort = whirl(a,wort);
 pump(a, true)
-pause(10)
+pause(5)
 pump(a, false)
 
 %Plate Chiller (1:1 output)
 wort = chill(a,wort);
 pump(a, true)
-pause(10)
+pause(5)
 pump(a, false)
 
 %Fermenter
 beer = ferment(a, s, recipe, wort);
 pump(a, true)
-pause(10)
+pause(5)
 pump(a, false)
 
 %Storage
 store(beer);
 
-%exit(0)
-
+    %exit(0)
